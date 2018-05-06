eb4d2b5f82397e1d9d5d384df5085ef74c3af878
Yash Kesarwani <36482238+YASHYK47@users.noreply.github.com>
2018-05-07 04:06:04 +0530

    Update programtwo.cpp

--- programtwo.cpp
+++ programtwo.cpp
@@ -1,13 +1,8 @@
 #include<stdio.h>  //To check entered number is armstrong or not
 main()
 {
-<<<<<<< HEAD
 int n,a,b,c,d,s;
 printf("Enter your number");
-=======
-intn,a,b,c,d,s;
-printf("Enter your number--");
->>>>>>> 681074270cbf6d74dd3cd864f0bcc9f55a446749
 scanf("%d",&n);
   a=n%10;
   b=n/10;
